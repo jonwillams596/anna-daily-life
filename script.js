4aaccd55e97db6b3a383502d092a963765977cc3
jonwillams596 <jonwillams596@gmail.com>
2025-05-07 21:24:33 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const cat = document.getElementById('cat');
-const input = document.getElementById('noteInput');
-
-input.addEventListener('input', () => {
-    cat.style.left = Math.random() * 100 + 'px';
-    cat.style.top = Math.random() * 50 + 'px';
-});
