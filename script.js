1b6e1cff4dcc6c35fa6c52edb3f5dc27d0eedc0f
jonwillams596 <jonwillams596@gmail.com>
2025-05-07 22:37:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-document.querySelector('.cat').addEventListener('click', () => alert('Meow! I am G the cat!'));
+const cat = document.querySelector('.cat');
+
+cat.addEventListener('click', () => {
+    alert('Meow! I am G the cat!! I’m so cute, right?');
+});
+
+function switchCatImage() {
+    cat.src = 'cat_walk.gif';
+    
+    setTimeout(() => {
+        cat.src = 'cat_blink.gif';
+    }, 2500);
+    
+    setTimeout(() => {
+        cat.src = 'cat_walk.gif';
+    }, 4000);
+    
+    setTimeout(() => {
+        cat.src = 'cat_yawn.gif';
+    }, 7000);
+    
+    setTimeout(() => {
+        cat.src = 'cat_walk.gif';
+    }, 9000);
+}
+
+switchCatImage();
+setInterval(switchCatImage, 10000);
